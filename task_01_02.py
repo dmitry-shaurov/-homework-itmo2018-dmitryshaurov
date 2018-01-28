107c21a764a728a5e14e4b151cafa72fdd59bdc5
Dmitry Shaurov <dmitry.shaurov@gmail.com>
2018-01-28 20:49:00 +0300

    1st homework v2

--- task_01_02.py
+++ task_01_02.py
@@ -3,12 +3,12 @@ detergent = float(input())
 while True:
     if plates / detergent > 2:
         N = plates - detergent / 0.5
-        print(">>> Моющее средство закончилось. Осталось " + str(int(N)) + " тарелок")
+        print("Моющее средство закончилось. Осталось " + str(int(N)) + " тарелок")
         break
     elif plates / detergent < 2:
         N = (detergent / 0.5 - plates)/2
-        print(">>> Все тарелки вымыты. Осталось " + str(N) + " ед. моющего средства")
+        print("Все тарелки вымыты. Осталось " + str(N) + " ед. моющего средства")
         break
     elif plates % detergent == 0:
-        print(">>> Все тарелки вымыты, моющее средство закончилось")
+        print("Все тарелки вымыты, моющее средство закончилось")
         break
