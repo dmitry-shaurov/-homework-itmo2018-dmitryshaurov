187920df8447fed532e0d57b66b8db657eedff24
Dmitry Shaurov <dmitry.shaurov@gmail.com>
2018-02-13 22:09:53 +0300

    13.02.2018 +

---
+++ task_decorator_login_required.py
@@ -0,0 +1,27 @@
+import hashlib
+
+def make_token(username, password):
+    s = username + password
+    return hashlib.md5(s.encode()).hexdigest()
+
+def login_required(func):
+    def wrapper(*args, **kwargs):
+        i = 0
+        while i < 3 :
+            login = input("\nLogin:")
+            password = input("Password:")
+            with open('token.txt') as token:
+                if make_token(login, password) != token.read():
+                    i += 1
+                else:
+                    return func(*args, **kwargs)
+        return None
+    return wrapper
+
+@login_required
+def hello():
+    print("Hello world!")
+
+#hello()
+# with open('token.txt', 'w') as f:
+#     f.write(make_token("username", "password"))
