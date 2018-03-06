fb2adbcd293e555426a2b7cb3b9bafd8a63cd1c3
Dmitry Shaurov <dmitry.shaurov@gmail.com>
2018-03-07 00:43:23 +0300

    07.03.2017 +task_menu.py

---
+++ task_menu.py
@@ -0,0 +1,66 @@
+from abc import ABCMeta, abstractmethod
+
+class CommandException(Exception):
+    # def __init__(self, *args, **kwargs):
+    #     Exception.__init__(self, *args, **kwargs)
+    pass
+class Command(metaclass=ABCMeta):
+    @abstractmethod
+    def execute(self, *args, **kwargs):
+        pass
+
+class Menu(object):
+    commands = {}
+    index = 0
+
+    @classmethod
+    def add_command(cls, name, klass):
+        if not name:
+            raise CommandException("Command must have a name!")
+        if not issubclass(klass, Command):
+            raise CommandException("Class {} is not a command!".format(klass))
+        cls.commands[name] = klass
+
+    @classmethod
+    def execute(cls, name, *args, **kwargs):
+        klass = cls.commands.get(name)
+        if klass is None:
+            raise CommandException("Command with name {} not found'".format(name))
+        return klass.execute
+
+    def __next__(self):
+        if self.index < len(self.commands.items()):
+            item = list(self.commands.items())[self.index]
+            self.index += 1
+            return item
+        else:
+            self.index = 0 # для дальнейшей возможности проитерировать объект
+            raise StopIteration()
+
+    def __iter__(self):
+        return self
+
+
+class ShowCommand(Command):
+    def __init__(self, task_id):
+        pass
+    def execute(self):
+        pass
+
+class ListCommand():
+    def __init__(self):
+        pass
+    def execute(self):
+        pass
+
+
+menu = Menu()
+menu.add_command("show", ShowCommand)
+menu.add_command("list", ListCommand)
+#menu.execute("something")
+
+for name, command in menu:
+    print(name, command)
+
+for item in menu:
+    print(item)
